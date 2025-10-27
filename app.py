ca38b19a87031179e4d828ccff47d6d1d1aa1334
Juliana Cardozo <juliana.boubee@gmail.com>
2025-10-27 17:27:39 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "API Flask funcionando!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
