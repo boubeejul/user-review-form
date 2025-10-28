12409d845eef4a9bd63c97864056e8a6f7908088
boubeejul <juliana.boubee@gmail.com>
2025-10-28 15:18:07 -0300

    creates routes and organize css

--- app/app.py
+++ app/app.py
@@ -1,11 +1,21 @@
-# app.py
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return jsonify({"message": "API Flask funcionando!"})
+def index():
+    #TODO: get reviews from api
+    return render_template("index.html")
+
+@app.route('/submit-review', methods=['POST'])
+def submit_review():
+    title = request.form.get("review-title")
+    username = request.form.get("review-username")
+    message = request.form.get("review-message")
+    
+    #TODO: post review to api
+    return redirect("/")
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
